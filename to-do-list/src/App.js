625fb405abdd1518bd5e10b8d39499e8ad31ddd3
ElisaChicos <veronica.chicos@info.uaic.ro>
2022-03-11 15:22:42 +0200

    SearchBar

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
 import search from './search.png';
 import './App.css';
 import { useState } from 'react';
+import pokemon from './pokemon.json';
+
+// function searchBar(){
+//   const [searchTermn, setSearchTerm] = useState('');
+//   return(
+
+//   );
+// }
+
 
 function block(name,number,obj1,obj2,img){
   const green={
@@ -9,8 +17,10 @@ function block(name,number,obj1,obj2,img){
   }
   return(
     <div>
+      <div className="container">
       <div className="name">{name}</div>
       <div className="number">{number}</div>
+      </div>
       <div className="container">
       <div className="text-block">
       <div className="blocks1">{obj1}</div>
@@ -25,6 +35,7 @@ function block(name,number,obj1,obj2,img){
 }
 
 function App() {
+  const [searchTermn, setSearchTerm] = useState('');
   const mystyle = {
     width :30,
     height: 30
@@ -34,32 +45,84 @@ function App() {
       <div>
       <h2 className="Titlu">Pokedex</h2>
       <div class="container">
-      {/* <img className="Img" src={search} alt="Search" style={mystyle}/> */}
-      <input className="InputBox" type={"text"} placeholder="Search pokemon name, number or type" >
+      {/* <img src={search} alt="Search" style={mystyle}/> */}
+      <input className="InputBox" type={"text"} onChange={event=> {setSearchTerm(event.target.value)}} placeholder="Search pokemon name, number or type" >
       </input>
       </div>
-      <div className='block-display'>
-      <p className="pokemonBox" style={{backgroundColor:'rgb(100,190,50)'}}>{block("Bulbasaur","#001","Grass","Poison","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png") }</p>
-      <p className="pokemonBox" style={{backgroundColor:'rgb(100,190,50)'}}>{block("Ivysaur","#002","Grass","Poison","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png")} </p>
-      <p className="pokemonBox" style={{backgroundColor:'rgb(100,190,50)'}}>{block("Venusaur","#003","Grass","Poison","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png")}</p>
-      
-      <p className="pokemonBox" style={{backgroundColor:'rgb(204,105,0)'}}>{block("Charmander","#004","Fire","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png")} </p>
-      <p className="pokemonBox" style={{backgroundColor:'rgb(204,105,0)'}}>{block("Charmeleon","#005","Fire","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png")} </p>
-      <p className="pokemonBox" style={{backgroundColor:'rgb(204,105,0)'}}>{block("Charizard","#006","Fire","Flying","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png")}</p>
-      
-      <p className="pokemonBox" style={{backgroundColor:'rgb(90,172,228)'}}>{block("Squirtle","#007","Water","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png")} </p>
-      <p className="pokemonBox" style={{backgroundColor:'rgb(90,172,228)'}}>{block("Wartortle","#008","Water","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png")} </p>
-      <p className="pokemonBox" style={{backgroundColor:'rgb(90,172,228)'}}>{block("Blastoise","#009","Water","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png")}</p>
-      
-      <p className="pokemonBox" style={{backgroundColor:'rgb(250,220,70)'}}>{block("Pikachu","#025","Electric","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png")} </p>
-      <p className="pokemonBox" style={{backgroundColor:'rgb(162,140,95)'}}>{block("Eevee","#133","Normal","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png")}</p> 
-      <p className="pokemonBox" style={{backgroundColor:'rgb(155,95,162)'}}>{block("Gengar","#094","Ghost","Poison","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png")}</p>
-      </div>
-      
+      {pokemon.filter((val)=>{
+        if(searchTermn == "")
+        return val
+        else if(val.name.toLocaleLowerCase().includes(searchTermn.toLocaleLowerCase()) || val.id==searchTermn || val.types[0].type.name.toLocaleLowerCase().includes(searchTermn.toLocaleLowerCase()))
+        {
+          return val
+        }
+      }).map((val,key)=>{
+        if(val.id==1 || val.id==2 ||val.id==3)
+        {
+          return <div className='block-display'>
+                  <p className="pokemonBox" style={{backgroundColor:'rgb(100,190,50)'}}>{block(val.name,val.id,val.types[0].type.name,val.types[0].type.name,val.sprites.other.official_artwork.front_default) }</p>
+                  </div>
+        }
+        else 
+        if( val.id == 4 || val.id == 5 || val.id ==6)
+          {
+            return <div className='block-display'>
+                    <p className="pokemonBox" style={{backgroundColor:'rgb(204,105,0)'}}>{block(val.name,val.id,val.types[0].type.name,val.types[0].type.name,val.sprites.other.official_artwork.front_default) }</p>
+                    </div>
+          }
+          else
+            if( val.id == 7 || val.id == 8 || val.id == 9)
+            {
+              return <div className='block-display'>
+                    <p className="pokemonBox" style={{backgroundColor:'rgb(90,172,228)'}}>{block(val.name,val.id,val.types[0].type.name,val.types[0].type.name,val.sprites.other.official_artwork.front_default) }</p>
+                    </div>
+            }
+            else
+              if(val.id == 25)
+                {
+                  return <div className='block-display'>
+                    <p className="pokemonBox" style={{backgroundColor:'rgb(250,220,70)'}}>{block(val.name,val.id,val.types[0].type.name,val.types[0].type.name,val.sprites.other.official_artwork.front_default) }</p>
+                    </div>
+                }
+                else
+                  if(val.id == 133)
+                    {
+                      return <div className='block-display'>
+                      <p className="pokemonBox" style={{backgroundColor:'rgb(162,140,95)'}}>{block(val.name,val.id,val.types[0].type.name,val.types[0].type.name,val.sprites.other.official_artwork.front_default) }</p>
+                      </div>
+                    }
+                    else
+                      if(val.id ==94)
+                      {
+                        return <div className='block-display'>
+                        <p className="pokemonBox" style={{backgroundColor:'rgb(155,95,162)'}}>{block(val.name,val.id,val.types[0].type.name,val.types[0].type.name,val.sprites.other.official_artwork.front_default) }</p>
+                        </div>
+                      }})
+      }
+        {/* Css inainte de functionalitate js
+        <div className='block-display'>
+        <p className="pokemonBox" style={{backgroundColor:'rgb(100,190,50)'}}>{block("Bulbasaur","#001","Grass","Poison","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png") }</p>
+        <p className="pokemonBox" style={{backgroundColor:'rgb(100,190,50)'}}>{block("Ivysaur","#002","Grass","Poison","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png")} </p>
+        <p className="pokemonBox" style={{backgroundColor:'rgb(100,190,50)'}}>{block("Venusaur","#003","Grass","Poison","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png")}</p>
+        
+        <p className="pokemonBox" style={{backgroundColor:'rgb(204,105,0)'}}>{block("Charmander","#004","Fire","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png")} </p>
+        <p className="pokemonBox" style={{backgroundColor:'rgb(204,105,0)'}}>{block("Charmeleon","#005","Fire","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png")} </p>
+        <p className="pokemonBox" style={{backgroundColor:'rgb(204,105,0)'}}>{block("Charizard","#006","Fire","Flying","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png")}</p>
+        
+        <p className="pokemonBox" style={{backgroundColor:'rgb(90,172,228)'}}>{block("Squirtle","#007","Water","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png")} </p>
+        <p className="pokemonBox" style={{backgroundColor:'rgb(90,172,228)'}}>{block("Wartortle","#008","Water","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png")} </p>
+        <p className="pokemonBox" style={{backgroundColor:'rgb(90,172,228)'}}>{block("Blastoise","#009","Water","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png")}</p>
+        
+        <p className="pokemonBox" style={{backgroundColor:'rgb(250,220,70)'}}>{block("Pikachu","#025","Electric","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png")} </p>
+        <p className="pokemonBox" style={{backgroundColor:'rgb(162,140,95)'}}>{block("Eevee","#133","Normal","","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/133.png")}</p> 
+        <p className="pokemonBox" style={{backgroundColor:'rgb(155,95,162)'}}>{block("Gengar","#094","Ghost","Poison","https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png")}</p>
+        </div>
+       */}
     </div>
-
-    
   );
 }
 
 export default App;
+/*
+
+*/
